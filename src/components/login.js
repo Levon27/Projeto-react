c290896f0779b7101aac2c4181913388225e03a8
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-12-13 12:47:12 -0200

    Login com firebase

--- src/components/login.js
+++ src/components/login.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
 import { View, Text,TextInput,Button,Alert } from 'react-native';
-import 'firebase/auth';
+
+import app from '@firebase/app';
+import '@firebase/auth';
+
 export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = { username:'',password:''
     };
+
+    const config = {
+      apiKey: "AIzaSyAvWfxUhCZIeBh4B8A7pQt0hKQAuMzbEIk",
+      authDomain: "projeto-react-f5b2c.firebaseapp.com",
+      databaseURL: "https://projeto-react-f5b2c.firebaseio.com",
+      projectId: "projeto-react-f5b2c",
+      storageBucket: "projeto-react-f5b2c.appspot.com",
+      messagingSenderId: "459346341374"
+    };
+    app.initializeApp(config);
+
   }
   
+ 
   logar(){
-	user = this.state.username
-	pass = this.state.password
+	  user = this.state.username;
+    pass = this.state.password; 
 
-  
+    app.auth().signInWithEmailAndPassword(user,pass).then(response => {Alert.alert("dados corretos")}).catch(err => {Alert.alert("dados errados")})
 
+    /*
+    Autenticação antiga
 
-  /*
-  Autenticação antiga
-  
-  user_login = ''
-  pass_login = ''
+    user_login = ''
+    pass_login = ''
 
-	if ((user == user_login) && (pass == pass_login)){
-		//Alert.alert('AVISO!','Login e senha corretos')
-		this.props.navigation.navigate('TelaLista')
-	}
-	else
-		Alert.alert('AVISO!','Dados incorretos!')
+    if ((user == user_login) && (pass == pass_login)){
+      //Alert.alert('AVISO!','Login e senha corretos')
+      this.props.navigation.navigate('TelaLista')
+    }
+    else
+      Alert.alert('AVISO!','Dados incorretos!')
+    }
+    */
   }
-  */
+
   render() {
     return (
       <View>
